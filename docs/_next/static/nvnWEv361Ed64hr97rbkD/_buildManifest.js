b261456079eac26b4e43eb47eb0c1f26a7905648
irfanbacker <irfuvk@gmail.com>
2020-12-16 15:15:29 +0530

    Fix missing files 4

---
+++ docs/_next/static/nvnWEv361Ed64hr97rbkD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F639dfc16e6d9fae83a8e.css","static\u002Fchunks\u002Fpages\u002Findex-c49fb7532558dab385c0.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6498da3276337ac2c18a.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
