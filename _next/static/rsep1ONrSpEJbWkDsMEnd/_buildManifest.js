16b0540c2c29a28f357258fa1f5154f2e82cf24c
irfanbacker <irfuvk@gmail.com>
2020-12-16 15:32:06 +0530

    Remove dev files

---
+++ _next/static/rsep1ONrSpEJbWkDsMEnd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F639dfc16e6d9fae83a8e.css","static\u002Fchunks\u002Fpages\u002Findex-c214b92c6a1a038e59e5.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6d7f10b6fb5a998345d3.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
